c74117fd1bf4c10465b16726fba99a3fd75e48e2
aileenhuang <yeeruhuang@gmail.com>
2019-08-06 12:08:32 -0400

    Clean up topic generator, add comments

--- normalize.py
+++ normalize.py
@@ -7,7 +7,7 @@ import simplejson as json
 import spacy
 
 from collections import Counter
-from lda import LDA, datasets
+from lda import LDA
 from multiprocessing.pool import ThreadPool
 from os import path
 from sklearn.feature_extraction.text import CountVectorizer
@@ -19,6 +19,9 @@ STOP_WORDS = spacy.lang.en.stop_words.STOP_WORDS
 
 
 def get_normalized_tokens(fname):
+    """
+    Normalizes (lemmatizes) text according to LDA's bag-of-words assumption
+    """
     with open(path.join(path.dirname(__file__), FILES_PATH, fname), "r", encoding="utf-8") as f:
         text = f.read().replace("\n", " ")  # Read in and replace newlines with space
         doc = nlp(text)
@@ -30,6 +33,9 @@ def get_normalized_tokens(fname):
 
 def get_document_term_matrix(file_to_tokens):
     """
+    Generates a document-term matrix, documents being the list of file names and terms
+    being the normalized set of unique tokens.
+
     Columns: words
     Rows: file names
     Cells: Frequencies
@@ -58,14 +64,19 @@ def generate_topics(np_matrix, all_tokens):
     plt.show()
 
 
-if __name__ == "__main__":
+def main(files):
     file_to_tokens = {}
     all_tokens = []
-    for f in TEST_FILES:  # TODO: Make this a little more robust with redundant file handling
+    for f in files:  # TODO: Make this a little more robust with redundant file handling
         tokens = get_normalized_tokens(f)
         all_tokens.extend(tokens)
         file_to_tokens[f] = " ".join(tokens)
-    all_tokens = list(set(all_tokens))
+
+    all_tokens = list(set(all_tokens))  # List of unique tokens
 
     np_matrix = get_document_term_matrix(file_to_tokens)
     generate_topics(np_matrix, all_tokens)
+
+
+if __name__ == "__main__":
+    main(TEST_FILES)
