4497c8d3cd1a782f7eeb4a8fa8a777835d9eca11
aileenhuang <yeeruhuang@gmail.com>
2019-08-07 22:50:51 -0400

    Hook file processor to application
    * Create uploaded file template
    * Generate output onto template
    * Call file processor from uploaded_files route

--- eigen-app/app.py
+++ eigen-app/app.py
@@ -1,13 +1,17 @@
-import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+"""A very simple Flask application"""
+
+from os import path
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+from .processors.topicgenerator import TopicGenerator
 from werkzeug.utils import secure_filename
 
 METHODS = ["GET", "POST"]
-UPLOAD_FOLDER = "/uploads"
+UPLOAD_FOLDER = path.join(path.abspath(path.dirname(__file__)), "uploads")
 ALLOWED_EXTENSIONS = {"txt"}
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 def is_valid_file(filename):
@@ -16,7 +20,7 @@ def is_valid_file(filename):
 
 def handle_upload(request):
     if "file" not in request.files:
-        flash("No file part")
+        flash("No file in request")
         return request.url
 
     file = request.files["file"]
@@ -24,9 +28,9 @@ def handle_upload(request):
         flash("No selected file")
         return request.url
 
-    if file and allowed_file(file.filename):
+    if file and is_valid_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        file.save(path.join(app.config["UPLOAD_FOLDER"], filename))
         return url_for("uploaded_file", filename=filename)
 
 
@@ -34,6 +38,12 @@ def handle_upload(request):
 def upload_file():
     if request.method == "POST":
         url = handle_upload(request)
-        redirect(url)
+        return redirect(url)
     else:
         return render_template("upload.html")
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    tg = TopicGenerator([filename])
+    tg.generate_gensim_topics()
+    return render_template("uploaded.html", tg=tg)
